5624f8b112bfe7c1675d2814420be952185bb82c
KC <kcemenike@github.com>
2022-08-23 13:09:49 -0300

    wait for user input

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 KEY=$(cut -d = -f 2 .env)
 # GET TOKEN
-curl --request POST \
+curl -s --request POST \
 --url https://dropmail.p.rapidapi.com/ \
 --header 'X-RapidAPI-Host: dropmail.p.rapidapi.com' \
 --header "X-RapidAPI-Key: $KEY" \
@@ -9,10 +9,14 @@ curl --request POST \
 
 EMAIL=$(cut -d : -f 10 creds.json | sed "s/}//g;s/]//;s/\"//g")
 TOKEN=$(cut -d : -f 4 creds.json | cut -d , -f 1 | sed "s/\"//g")
-echo email=$EMAIL
+echo Your new email is $EMAIL
 
-# Wait
-sleep 180s
+# Wait for user input
+echo "."
+echo ".."
+echo "..."
+echo ""
+read -n 1 -s -r -p "Now sign up with the email displayed above.\nAfter verification email is sent, press any key to continue..."
 
 # GET MESSAGE LIST
 curl -s --request POST \
@@ -29,6 +33,12 @@ SUBJECT=$(cut -d , -f 4 mailbox.json | cut -d : -f 2 | sed "s/\"//g")
 TEXT=$(cut -d , -f 2 mailbox.json | cut -d : -f 2 | sed "s/\"//g")
 FILE=$(cut -d , -f 6 mailbox.json | cut -d : -f 2,3,4,5 | sed "s/\"//g;s/}//g;s/]//g")
 curl -s $FILE >file.eml
+
+if [ $(wc -c mailbox.json | cut -d ' ' -f 1) -eq 33 ]; then
+    echo "\nNo email was received in the mailbox"
+else
+    echo "\nCheck the file.eml for the full email received in $TO mailbox. The email text is $TEXT"
+fi
 
 # python -c """
 # import sys, json;
